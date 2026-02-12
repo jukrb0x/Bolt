4e19de2fdddc5b5a299340d8d963dfd3f17ce464
Jabriel <jabrielah@outlook.com>
2026-02-12 15:30:00 +0800

    feat: logger with coloured output and optional file sink

---
+++ src/logger.ts
@@ -0,0 +1,46 @@
+import pc from "picocolors"
+import { createWriteStream, type WriteStream } from "fs"
+
+type Sink = (line: string) => void
+
+interface LoggerOptions {
+  sink?: Sink
+  logFile?: string
+}
+
+export class Logger {
+  private sink: Sink
+  private file?: WriteStream
+
+  constructor(opts: LoggerOptions = {}) {
+    this.sink = opts.sink ?? ((l) => process.stdout.write(l + "\n"))
+    if (opts.logFile) {
+      this.file = createWriteStream(opts.logFile, { flags: "a" })
+    }
+  }
+
+  info(msg: string)  { this.log("INFO",  (s) => pc.green(s),  msg) }
+  warn(msg: string)  { this.log("WARN",  (s) => pc.yellow(s), msg) }
+  error(msg: string) { this.log("ERROR", (s) => pc.red(s),    msg) }
+  debug(msg: string) { this.log("DEBUG", (s) => pc.dim(s),    msg) }
+
+  step(name: string) {
+    this.sink(pc.blue(`>> ${name}`))
+  }
+
+  success(name: string, dur: number) {
+    this.sink(`${pc.blue("<<")} ${name}: ${pc.green("SUCCESS")} (${dur.toFixed(1)}s)`)
+  }
+
+  fail(name: string, dur: number) {
+    this.sink(`${pc.blue("<<")} ${name}: ${pc.red("FAILED")} (${dur.toFixed(1)}s)`)
+  }
+
+  close() { this.file?.end() }
+
+  private log(level: string, colour: (s: string) => string, msg: string) {
+    const line = `[${colour(level)}] ${msg}`
+    this.sink(line)
+    this.file?.write(`[${level}] ${msg}\n`)
+  }
+}
