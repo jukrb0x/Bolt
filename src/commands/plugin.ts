378e6afbbc42f909db4eacd02650cd692c64052c
Jabriel <jabrielah@outlook.com>
2026-02-27 01:37:00 +0800

    feat: add bolt plugin command group (new, list)

---
+++ src/commands/plugin.ts
@@ -0,0 +1,11 @@
+import { defineCommand } from "citty";
+import pluginNewCmd from "./plugin-new";
+import pluginListCmd from "./plugin-list";
+
+export default defineCommand({
+  meta: { description: "Manage bolt plugins" },
+  subCommands: {
+    new: pluginNewCmd,
+    list: pluginListCmd,
+  },
+});
